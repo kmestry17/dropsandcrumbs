b62d4b014db242b0ec8cd5e27815d687987adcbb
Kunal Mestry <kmestry17@gmail.com>
2024-06-09 02:23:50 -0500

    navbar changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
+import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return (
     <div className="container">
       <h1>dropsandcrumbs</h1>
+      <Navbar />
     </div>
   );
 };
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,37 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar-main">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#gallery">
+            Gallery
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#recipe">
+            Recipes
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#about">
+            About
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#contact">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
